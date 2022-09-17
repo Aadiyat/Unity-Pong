738737bea722bf9517aae4d269838f82142fb46c
Aadiyat <aadiyat.ahmad@gmail.com>
2022-09-17 13:27:39 -0400

    setting rigidbody velocity instead of  using translate

--- Assets/Scripts/PaddleVerticalMovement.cs
+++ Assets/Scripts/PaddleVerticalMovement.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class PaddleVerticalMovement : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
+    private Rigidbody2D m_rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, Input.GetAxis("Vertical"), 0) * moveSpeed * Time.deltaTime);
+        m_rigidbody2d.velocity = new Vector3(0, Input.GetAxis("Vertical"), 0) * moveSpeed;
     }
 }
