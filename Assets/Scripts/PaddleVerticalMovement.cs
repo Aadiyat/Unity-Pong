8783c102c37e36489d7ca58fde4bac62daf61d14
Aadiyat <aadiyat.ahmad@gmail.com>
2022-09-18 18:48:20 -0400

    check paddle collisions with static objects

--- Assets/Scripts/PaddleVerticalMovement.cs
+++ Assets/Scripts/PaddleVerticalMovement.cs
@@ -5,16 +5,43 @@ using UnityEngine;
 public class PaddleVerticalMovement : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
-    private Rigidbody2D m_rigidbody2d;
+    public ContactFilter2D movementFilter;
+
+    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    private Rigidbody2D rigidBody;
+    private float collisionOffset = 0.05f;
+
     // Start is called before the first frame update
     void Start()
     {
-        m_rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        m_rigidbody2d.velocity = new Vector3(0, Input.GetAxis("Vertical"), 0) * moveSpeed;
+        updatePosition();
+    }
+
+    private void updatePosition()
+    {
+        Vector2 movementDirecton = new Vector2(0, Input.GetAxis("Vertical"));
+        bool willCollide = checkCollisions(movementDirecton);
+        Debug.Log(willCollide);
+        if(!willCollide) rigidBody.MovePosition(rigidBody.position + movementDirecton * moveSpeed * Time.deltaTime);
+    }
+
+    private bool checkCollisions(Vector2 movementDirection){
+        int numCollisions = rigidBody.Cast(
+            movementDirection,
+            movementFilter,
+            castCollisions,
+            moveSpeed * Time.fixedDeltaTime + collisionOffset
+        );
+
+       if (castCollisions.Exists(collider => collider.rigidbody.bodyType == RigidbodyType2D.Static))
+            return true;
+        
+        return false;
     }
 }
