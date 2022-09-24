8558fc5f7958159bfc6877fc7bc14a768a76551b
Aadiyat <aadiyat.ahmad@gmail.com>
2022-09-23 20:52:57 -0400

    added score  UI

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
     private int score = 0;
+    public Text scoreText;
+
+    void Start(){
+        scoreText.text= score.ToString();
+    }
 
     public void incrementScore(){
         score++;
+        scoreText.text = score.ToString();
     }
 
     public int getScore(){
